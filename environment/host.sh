9e0c0331a024cbe03d14cd3cc5e6b830130db755
lothrond <msm255243@gmail.com>
2020-01-22 09:26:32 -0500

    On branch dev

--- environment/host.sh
+++ environment/host.sh
@@ -4,13 +4,20 @@
 
 export PATH=~/bin:${PATH}:~/depot_tools
 
-# chromiumos source code
+# Chromium OS source code
 export SOURCE_REPO=~/chromiumos
 
 # repo sync
 export REPOFLAGS="-j8"
 
+## END CHROMIUM OS HOST BUILD ENVIRONMENT
+
+## BEGIN CHROMIUM HOST BUILD ENVIRONMENT:
+
+# Chromium source code
+CHROMIUM_DIR=~/chromium
+
+## END CHROMIUM HOST BUILD ENVIRONMENT.
+
 # misc
 export GIT_DISCOVERY_ACROSS_FILESYSTEMS=true
-
-## END CHROMIUM OS HOST BUILD ENVIRONMENT
