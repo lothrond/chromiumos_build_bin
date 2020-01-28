2c85155c99a838451e73b727c557689d2be7e9da
lothrond <msm255243@gmail.com>
2020-01-28 08:55:22 -0500

    On branch master

--- environment/chroot.sh
+++ environment/chroot.sh
@@ -4,11 +4,7 @@
 
 export PATH=~/bin:${PATH}
 export BOARD=cyan
-<<<<<<< HEAD
-#export BOARD_VARIANT=
-=======
 export IMAGE=base
 export VERITY=on
->>>>>>> dev
 
 ## END CHROMIUM OS BUILD CHROOT ENVIRONMENT.
